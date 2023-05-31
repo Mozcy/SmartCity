b05325ed273b72e76233518eda8f97515bd8a21e
Puppet <puppet.xiaoyi@gmail.com>
2023-05-31 19:00:53 +0800

    improvement: 改进 删除 kotlin 类里 package 后面的分号(;)

--- app/src/main/java/com/example/smartcity/fragment/MainFragmentNav1.kt
+++ app/src/main/java/com/example/smartcity/fragment/MainFragmentNav1.kt
@@ -1,4 +1,4 @@
-package com.example.smartcity.fragment;
+package com.example.smartcity.fragment
 
 import android.content.Intent
 import android.text.Html
--- app/src/main/java/com/example/smartcity/fragment/MainFragmentNav5.kt
+++ app/src/main/java/com/example/smartcity/fragment/MainFragmentNav5.kt
@@ -1,4 +1,4 @@
-package com.example.smartcity.fragment;
+package com.example.smartcity.fragment
 
 import android.app.Activity
 import android.content.Intent
